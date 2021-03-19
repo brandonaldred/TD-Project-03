51f544446afadbbbf61389c92af6978345413bdc
Brandon Aldred <brandonaldred@gmail.com>
2021-03-19 06:45:36 -0700

    Shift Selection Working

--- js/script.js
+++ js/script.js
@@ -9,23 +9,30 @@ jobRole.addEventListener('input', (e) => {
     jobRole.value === 'other' ? jobRoleOther.style.display = '' : jobRoleOther.style.display = 'none';
 });
 
-//Hide Designs until designs are clicked
+//Getting the shirt design value and updating color drop down
 const shirtDesign = document.getElementById('design');
-const shirtColor = document.getElementById('color');
-displayShirtColor(shirtDesign.value);
+displayShirtColor(shirtDesign.value, false);
 
-
-function displayShirtColor(data) {
+//Function to change shirt color options based on the design selected
+function displayShirtColor(data, change) {
+    const shirtColor = document.getElementById('color');
     for (let i = 0; i < shirtColor.length; i++) {
         let shirt = shirtColor[i];
-        if(shirt.getAttribute('data-theme') === data) {
+        if (shirt.getAttribute('data-theme') === data) {
             shirt.style.display = '';
         } else {
             shirt.style.display = 'none';
         }
     }
+    if (change) {
+        let pleaseSelect = document.createElement('option');
+        pleaseSelect.innerText = 'Select A Color';
+        shirtColor.prepend(pleaseSelect);
+        shirtColor.selectedIndex = 0;
+    }
 }
 
-shirtDesign.addEventListener('input', (e) => {
-    displayShirtColor(shirtDesign.value);
+//Adding event listener to the design selector and updating shirt color changes
+shirtDesign.addEventListener('change', () => {
+    displayShirtColor(shirtDesign.value, true);
 });

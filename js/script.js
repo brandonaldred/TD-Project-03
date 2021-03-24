0557918f43b9c69f578e9c3b7e23ca99963526f7
Brandon Aldred <brandonaldred@gmail.com>
2021-03-23 21:09:19 -0700

    Updated JS
    Updated the JS to filter out conflicting time slots when an activity checked

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 //Grab namefield & focus
-document.getElementById('name').focus();
+const userName = document.getElementById('name');
+userName.focus();
 
 //Grab email field
 let emailEl = document.getElementById('email');
@@ -50,24 +51,81 @@ shirtDesign.addEventListener('change', () => {
 
 
 //Add up selected courses
+//Get boxes checked value to know what to add or subtract
+const courses = document.getElementById('activities');
+const times = courses.querySelectorAll('input');
 const totalCost = document.getElementById('activities-cost');
 
-function addCost(amount, bool) {
+function addCost(amount, bool, time) {
     let total;
     let match = Number(totalCost.innerText.match(/\d+/));
     if (bool) {
         total = match + Number(amount);
+        removeTimeConflict(time);
     } else {
         total = match - Number(amount);
+        restoreActivities(time);
     }
     totalCost.innerHTML = `Total: $${total}`;
-
 }
 
+function removeTimeConflict (time) {
+   for (let i = 0; i < times.length; i++) {
+       if(times[i].getAttribute('data-day-and-time') === time && !times[i].checked) {
+           times[i].disabled = true;
+       }
+   }
+}
 
-//Get boxes checked value to know what to add or subtract
-const courses = document.getElementById('activities');
+function restoreActivities(time) {
+    for (let i = 0; i <times.length; i++) {
+        if(times[i].getAttribute('data-day-and-time') === time && times[i].disabled) {
+            times[i].disabled = false;
+        }
+    }
+}
+
 courses.addEventListener('change', (e) => {
+    let time = e.target.getAttribute('data-day-and-time');
     let cost = e.target.getAttribute('data-cost');
-    e.target.checked ? addCost(cost, true) : addCost(cost, false);
+    e.target.checked ? addCost(cost, true, time) : addCost(cost, false, time);
+});
+
+
+
+
+
+
+
+//Change/hide fields depending on payment method selected
+function paymentMethods (type) {
+    if (type === 'credit-card') {
+        document.getElementById('credit-card').style.display = '';
+        document.getElementById('paypal').style.display = 'none';
+        document.getElementById('bitcoin').style.display = 'none';
+    } else if(type === 'paypal') {
+        document.getElementById('credit-card').style.display = 'none';
+        document.getElementById('paypal').style.display = '';
+        document.getElementById('bitcoin').style.display = 'none';
+    } else if(type === 'bitcoin') {
+        document.getElementById('credit-card').style.display = 'none';
+        document.getElementById('paypal').style.display = 'none';
+        document.getElementById('bitcoin').style.display = '';
+    }
+}
+
+const paymentMethod = document.getElementById('payment');
+paymentMethod[1].selected = true;
+paymentMethods(paymentMethod[1].value);
+paymentMethod.addEventListener('change', (e) => {
+    paymentMethods(paymentMethod.value);
+});
+
+const form = document.getElementsByTagName('form');
+const submit = document.getElementsByTagName('button');
+submit[0].addEventListener('click', (e) => {
+    if (!userName.value) {
+        console.log('enter name');
+        e.preventDefault();
+    }    
 });

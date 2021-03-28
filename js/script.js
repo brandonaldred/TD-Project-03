b1f5780a3c2acb51547ebe18297461e0121a3aca
Brandon Aldred <brandonaldred@gmail.com>
2021-03-28 14:44:24 -0700

    adding images and updated script

--- js/script.js
+++ js/script.js
@@ -19,13 +19,18 @@ function enterValidEmail() {
 }
 
 //Validate Email Address
+
+//Split into 3 matches by excluding @ and . before com or net, etc.
 function emailValidate(email) {
     const regEx = /^\w*@\w*\.\w{2,}$/g;
     if (!regEx.test(email)) {
+        ;
         enterValidEmail();
     } else {
         const span = document.querySelector('.enter-valid-email');
-        span.parentNode.removeChild(span);
+        if (span) {
+            span.parentNode.removeChild(span);
+        }
     }
 }
 
@@ -151,4 +156,119 @@ submit[0].addEventListener('click', (e) => {
         console.log('enter name');
         e.preventDefault();
     }
+});
+
+
+//Credit card validation
+const date = new Date();
+const month = date.getMonth() + 1;
+const year = date.getFullYear();
+
+function removeExpYear(selectedMonth) {
+    const expYear = document.getElementById('exp-year');
+    const options = expYear.querySelectorAll('option');
+    if (options[1].value == year && selectedMonth < month) {
+        options[1].hidden = true;
+    } else {
+        options[1].hidden = false;
+    }
+
+}
+
+const expMonth = document.getElementById('exp-month');
+const currentYear = document.createElement('option');
+expMonth.addEventListener('change', (e) => {
+    removeExpYear(e.target.value);
+});
+
+//Validate CC Number
+function ccValidate(ccNumber,format) {
+    const regEx = /^(\d{4})-?(\d{4})-?(\d{4})-?(\d{4})$/g;
+    if (!regEx.test(ccNumber)) {
+        ;
+        enterValidCC();
+    } else {
+        const span = document.querySelector('.enter-valid');
+        if (span) {
+            span.parentNode.removeChild(span);
+            format.value = `$1-$2-$3-$4`;
+        }
+    }
+}
+
+//Creating an element to enter for valid CC
+function enterValidCC() {
+    const emailHint = document.getElementById('cc-hint');
+    const span = document.createElement('span');
+    span.className = 'enter-valid';
+    if (emailHint.parentNode.querySelectorAll('.enter-valid').length === 0) {
+        span.style.color = 'red';
+        span.textContent = 'Please insert a valid credit card number';
+        emailHint.parentNode.insertBefore(span, emailHint);
+    }
+}
+
+const creditCardNum = document.getElementById('cc-num');
+creditCardNum.addEventListener('input', (e) => {
+    let ccNumber =  e.target.value;
+    let format = e.target;
+    ccValidate(ccNumber, format);
+});
+
+
+/////Custom CC Validator with card type
+const input = document.querySelector('input');
+const div = document.querySelector('div');
+const p = document.querySelector('p');
+
+const ccRegEx = /^(\d{4})[ -]?(\d{4})[ -]?(\d{4})[ -]?(\d{4})$/;
+const amRegEx = /^(\d{4})[ -]?(\d{6})[ -]?(\d{5})/;
+
+function ccFormat(ccNumber) {
+    let type = '';
+    div.textContent = '';
+    if (/^4/.test(ccNumber)) {
+        type = 'Visa'
+    }
+    if (/^6/.test(ccNumber)) {
+        type = 'Discover'
+    }
+    if (/^34/.test(ccNumber) || /^37/.test(ccNumber)) {
+        type = 'American express';
+    }
+    if (/^5[1-5]/.test(ccNumber)) {
+        type = 'Mastercard';
+    }
+    if (!ccNumber || /\D/.test(ccNumber)) {
+        type = 'A valid credit card is required';
+    }
+    p.textContent = type;
+    let output = '';
+    if ((type === 'Visa' || type === 'Mastercard' || type === 'Discover') && ccNumber.length === 16) {
+        output = ccNumber.match(ccRegEx);
+        div.textContent = `${output[1]}-${output[2]}-${output[3]}-${output[4]}`
+    } else if (type === 'American express' && ccNumber.length === 15) {
+        output = ccNumber.match(amRegEx);
+        div.textContent = `${output[1]}-${output[2]}-${output[3]}`;
+    } else {
+        if (!output && type === 'Visa') {
+            div.textContent = 'Please enter a valid Visa credit card number';
+        }
+        if (!output && type === 'American express') {
+            div.textContent = 'Please enter a valid American Express credit card number';
+        }
+        if (!output && type === 'Mastercard') {
+            div.textContent = 'Please enter a valid Mastercard credit card number';
+        }
+    }
+}
+
+//To be applied
+// background: url('') no-repeat 0;
+// background-size: contain;
+// background-position: right;
+// padding-right: 75px;
+
+input.addEventListener('input', (e) => {
+    ccFormat(e.target.value);
 });
